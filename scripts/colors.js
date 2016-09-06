d21ff29a1e7b0c45c84eaa7232e3d3a7f8cf420d
Brent Reno <brent.reno@gmail.com>
2016-09-06 14:29:05 -0500

    Red click and counter working

--- scripts/colors.js
+++ scripts/colors.js
@@ -1,14 +1,25 @@
 console.log("Colors.js is working");
-////////Global Variables\\\\\\\\\\\\\\
-var redClicks =0;
-var blueClicks =0;
-var yellowClicks =0;
-var greenClicks =0;
+
+
 $(document).ready(function(){
   console.log('JQ is loaded');
+  //establish Click counters
+  var redClicks =0;
+  var blueClicks =0;
+  var yellowClicks =0;
+  var greenClicks =0;
   //Create red div on click
   $('[data-color="red"]').on('click', function(){
     console.log('The red button was clicked');
+    //create red div
+    var redDiv='<div class= "color-cube red"></div>';
+    //append red div to the container
+    $('.container').append(redDiv);
+    //incrament red clicks
+    redClicks++;
+    console.log('redClicks:', redClicks);
+    //update total red paragraph
+    $('#red').html('Total red: ' + redClicks);
   });
   //Create blue div on click
   $('[data-color="blue"]').on('click', function(){
