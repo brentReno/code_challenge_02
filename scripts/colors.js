a5258577fa311600535971761f952a715b9072a7
Brent Reno <brent.reno@gmail.com>
2016-09-06 14:17:44 -0500

    initial commit

---
+++ scripts/colors.js
@@ -0,0 +1,15 @@
+console.log("Colors.js is working");
+////////Global Variables\\\\\\\\\\\\\\
+var redClicks =0;
+var blueClicks =0;
+var yellowClicks =0;
+var greenClicks =0;
+$(document).ready(function(){
+  console.log('JQ is loaded');
+  //Create red div on click
+  
+  //Create blue div on click
+  //create yellow div on click
+  //create green div on click
+
+});
