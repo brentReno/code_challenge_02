af72e68fde8b5410f50512b756d6a84f48743db7
Brent Reno <brent.reno@gmail.com>
2016-09-06 14:23:20 -0500

    handshake between JS and buttons established

--- scripts/colors.js
+++ scripts/colors.js
@@ -7,9 +7,20 @@ var greenClicks =0;
 $(document).ready(function(){
   console.log('JQ is loaded');
   //Create red div on click
-  
+  $('[data-color="red"]').on('click', function(){
+    console.log('The red button was clicked');
+  });
   //Create blue div on click
+  $('[data-color="blue"]').on('click', function(){
+    console.log('The blue button was clicked');
+  });
   //create yellow div on click
+  $('[data-color="yellow"]').on('click', function(){
+    console.log('The yellow button was clicked');
+  });
   //create green div on click
+  $('[data-color="green"]').on('click', function(){
+    console.log('The green button was clicked');
+  });
 
 });
